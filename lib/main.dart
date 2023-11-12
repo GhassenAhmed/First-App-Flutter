15e580436887083bb8ed25e90d4a3aed13c8c97c
ghassen <ghassenahmed74@gmail.com>
2023-11-12 21:14:42 +0100

    add widget column

--- lib/main.dart
+++ lib/main.dart
@@ -11,43 +11,59 @@ class RollDiceWidget extends StatelessWidget {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-          appBar: AppBar(
-            elevation: 10,
-            title: const Text("Facbook"),
-            backgroundColor: const Color.fromARGB(185, 20, 121, 230),
-            centerTitle: true,
-            leading: IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.menu,
-                  size: 33,
-                )),
-            actions: [
-              IconButton(
+            appBar: AppBar(
+              elevation: 10,
+              title: const Text("Facbook"),
+              backgroundColor: const Color.fromARGB(185, 20, 121, 230),
+              centerTitle: true,
+              leading: IconButton(
                   onPressed: () {},
                   icon: const Icon(
-                    Icons.message,
-                    size: 25,
+                    Icons.menu,
+                    size: 33,
                   )),
-              IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.search,
-                    size: 28,
-                  ))
-            ],
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () => {},
-            child: const Icon(Icons.add),
-          ),
-          body: const Text(
-            "Hello DSI32",
-            style: TextStyle(fontSize: 25, backgroundColor: Colors.deepOrange),
-            maxLines: 2,
-            overflow: TextOverflow.fade,
-            textAlign: TextAlign.left,
-          ),
-        ));
+              actions: [
+                IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.message,
+                      size: 25,
+                    )),
+                IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.search,
+                      size: 28,
+                    ))
+              ],
+            ),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () => {},
+              child: const Icon(Icons.add),
+            ),
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(
+                  "ghassen ahmed",
+                  style: TextStyle(color: Colors.black, fontSize: 33),
+                ),
+                const Text(
+                  "dsi32",
+                  style: TextStyle(
+                      color: Colors.amberAccent,
+                      fontSize: 22,
+                      backgroundColor: Colors.blueAccent),
+                ),
+                IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.access_alarm,
+                      size: 40,
+                      color: Colors.deepOrange,
+                    ))
+              ],
+            )));
   }
 }
